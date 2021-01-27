045c7eb5826101b6b7dccebcbb79866f2cbcc716
GilleM <matea.leka@gmail.com>
2021-01-27 23:18:26 +0000

    Flip and timer (icon) set (phone responsiveness not fixed!)

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,3 +1,28 @@
+// background music set
+class AudioController {
+   constructor() {
+        this.backgroundMusic = new Audio ("assets/music/game_music.mp3");
+        this.backgroundMusic.volume = 0.5;
+        this.backgroundMusic.loop = true;
+   }
+
+   startMusic() {
+       this.backgroundMusic.play(); 
+   }
+   stopMusic() {
+       this.backgroundMusic.pause();
+       this.backgroundMusic.currentTime=0; 
+   }
+   victory() {
+       this.stopMusic(); 
+   }
+   gameOver() {
+       this.backgroundMusic.play(); 
+   }
+}
+
+
+
 // GRABBING OVERLAYS and CARD FROM THE DOM 
 // 3 overlays: START, GAME OVER, VICTORY
 // decided not to go with HTML collection in order to have access to JavaScript array functions
@@ -25,10 +50,6 @@ function ready(){
 
 
 
-
-
-
-
 //if the html page hasn't loaded yet, wait for the page to get loaded and then call the function ready()
 
 if (document.readyState === "loading") {
@@ -38,16 +59,4 @@ if (document.readyState === "loading") {
 }
 
 
-// background music set
-class AudioController {
-   constructor() {
-        this.backgroundMusic = new Audio ("assets/music/game_music.mp3");
-        this.backgroundMusic.volume = 0.5;
-        this.backgroundMusic.loop = true;
-   }
-
-   startMusic() {
-       this.backgroundMusic.play(); 
-   }
-}
 
