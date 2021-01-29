722b2b453e32f351dad827dc329da92de2debd26
GilleM <matea.leka@gmail.com>
2021-01-29 02:21:33 +0000

    Light/Dark mode done, it toggles and the background changes on click

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,62 +1,13 @@
-// background music set
-class AudioController {
-   constructor() {
-        this.backgroundMusic = new Audio ("assets/music/game_music.mp3");
-        this.backgroundMusic.volume = 0.5;
-        this.backgroundMusic.loop = true;
-   }
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
 
-   startMusic() {
-       this.backgroundMusic.play(); 
-   }
-   stopMusic() {
-       this.backgroundMusic.pause();
-       this.backgroundMusic.currentTime=0; 
-   }
-   victory() {
-       this.stopMusic(); 
-   }
-   gameOver() {
-       this.backgroundMusic.play(); 
-   }
+// Switch theme
+function switchTheme(event) {
+    if (event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
 }
 
-
-
-// GRABBING OVERLAYS and CARD FROM THE DOM 
-// 3 overlays: START, GAME OVER, VICTORY
-// decided not to go with HTML collection in order to have access to JavaScript array functions
-function ready(){
-//makes array of elements
-    let overlaysArray = Array.from(document.querySelectorAll(".overlay-text"));
-    //let cardArray = Array.from(document.querySelectorAll(".memory-card"));
-// looping over arrays 
-    overlaysArray.forEach(overlay => {
-        overlay.addEventListener('click', () => {
-            overlay.classList.remove ("visible")
-            let audioController = new AudioController();
-            audioController.startMusic();
-            });    // WE STILL NEED TO INITIALISE THE GAME!!
-          
-        });
-// FLIPPING CARDS when we click it
-    const cards = document.querySelectorAll('.memory-card');
-
-    function cardFlip() {
-        this.classList.toggle("flip");
-        }
-    cards.forEach(card => card.addEventListener('click', cardFlip))
-}
-
-
-
-//if the html page hasn't loaded yet, wait for the page to get loaded and then call the function ready()
-
-if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", ready());
-} else {
-    ready
-}
-
-
-
+// Event listener
+toggleSwitch.addEventListener('change', switchTheme);
---
+++ assets/script/app2.js
@@ -0,0 +1,62 @@
+// background music set
+class AudioController {
+   constructor() {
+        this.backgroundMusic = new Audio ("assets/music/game_music.mp3");
+        this.backgroundMusic.volume = 0.5;
+        this.backgroundMusic.loop = true;
+   }
+
+   startMusic() {
+       this.backgroundMusic.play(); 
+   }
+   stopMusic() {
+       this.backgroundMusic.pause();
+       this.backgroundMusic.currentTime=0; 
+   }
+   victory() {
+       this.stopMusic(); 
+   }
+   gameOver() {
+       this.backgroundMusic.play(); 
+   }
+}
+
+
+
+// GRABBING OVERLAYS and CARD FROM THE DOM 
+// 3 overlays: START, GAME OVER, VICTORY
+// decided not to go with HTML collection in order to have access to JavaScript array functions
+function ready(){
+//makes array of elements
+    let overlaysArray = Array.from(document.querySelectorAll(".overlay-text"));
+    //let cardArray = Array.from(document.querySelectorAll(".memory-card"));
+// looping over arrays 
+    overlaysArray.forEach(overlay => {
+        overlay.addEventListener('click', () => {
+            overlay.classList.remove ("visible")
+            let audioController = new AudioController();
+            audioController.startMusic();
+            });    // WE STILL NEED TO INITIALISE THE GAME!!
+          
+        });
+// FLIPPING CARDS when we click it
+    const cards = document.querySelectorAll('.memory-card');
+
+    function cardFlip() {
+        this.classList.toggle("flip");
+        }
+    cards.forEach(card => card.addEventListener('click', cardFlip))
+}
+
+
+
+//if the html page hasn't loaded yet, wait for the page to get loaded and then call the function ready()
+
+if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", ready());
+} else {
+    ready
+}
+
+
+
