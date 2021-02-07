fb07e8d6c0aaae04d8ba731b6ee6ca9f02ebfe79
GilleM <matea.leka@gmail.com>
2021-02-07 22:38:41 +0000

    Getting rid of Flip&Time constructor; declaring variables in ready func.

--- assets/script/app2.js
+++ assets/script/app2.js
@@ -1,17 +1,87 @@
-// Setting up Timer and Flips
-class FlipAndTime {
-    constructor(totalTime, cards) {
-        this.cardsArray = cards;
-        this.totalTime = totalTime;
-        this.timeRemaining = totalTime;
-        this.timer = document.getElementById('time-remaining');
-        this.ticker = document.getElementById('flips');
-        this.audioController = new AudioController();
+// background music set
+class AudioController {
+    constructor() {
+        this.backgroundMusic = new Audio("assets/music/game_music.mp3");
+        this.gameOverSong = new Audio("assets/music/game_over_song.mp3");
+        this.victorySong = new Audio("assets/music/victory_song.mp3");
+        this.backgroundMusic.volume = 0.5;
+        this.gameOverSong.volume = 0.2;
+        this.backgroundMusic.loop = true;
     }
 
+    startMusic() {
+        this.backgroundMusic.play();
+    }
+    stopMusic() {
+        this.backgroundMusic.pause();
+        this.backgroundMusic.currentTime = 0;
+    }
+    victory() {
+        this.stopMusic();
+        this.victorySong.play();
+    }
+    gameOver() {
+        this.stopMusic();
+        this.gameOverSong.play();
+    }
+}
+
+function ready() {
+    let overlaysArray = Array.from(document.querySelectorAll(".overlay-text"));
+    let cardsArray = Array.from(document.querySelectorAll(".memory-card"));
+    let game;
+    let totalTime;
+
+    let matchedCard = [];
+    let busy = true;
+
+
+    let theCardIsFlipped;
+    let firstCard, secondCard;
+    // lockBoard = not to match another pair before the first pair matches (unflips) or unmatches (flips back)
+    let lockBoard = false;
+
+    let totalClicks = 0;
+    let cardToCheck = null;
+    let countDown;
+    let timeRemaining = totalTime;
+    let timer = document.getElementById('time-remaining');
+    let ticker = document.getElementById('flips');
+    let audioController = new AudioController();
+
+
+    const pathName = window.location.pathname;
+    const gameType = pathName.indexOf("easy") !== -1 ? "EASY" :
+        pathName.indexOf("hard") !== -1 ? "HARD" : "MEDIUM";
+
+    if (gameType === "EASY") {
+        totalTime = 120;
+    } else if (gameType === "MEDIUM") {
+        totalTime = 90;
+    } else if (gameType === "HARD") {
+        totalTime = 60;
+    }
+
+
+    overlaysArray.forEach(overlay => {
+        overlay.addEventListener('click', () => {
+            overlay.classList.remove("visible");
+            startGame();
+        });
+    });
+
+    cardsArray.forEach(card => {
+        card.addEventListener('click', () => {
+            flipCard(card);
+        });
+    });
+
+    //=====================================
+    const cards = document.querySelectorAll('.memory-card');
+
+
     startGame() {
-        this.totalClicks = 0;
-        this.cardToCheck = null;
+
         // we will put all the matching cards into matchedCard array
         this.matchedCards = [];
         this.busy = true;
@@ -120,135 +190,73 @@ class FlipAndTime {
 }
 
 
-// background music set
-class AudioController {
-    constructor() {
-        this.backgroundMusic = new Audio("assets/music/game_music.mp3");
-        this.gameOverSong = new Audio("assets/music/game_over_song.mp3");
-        this.victorySong = new Audio("assets/music/victory_song.mp3");
-        this.backgroundMusic.volume = 0.5;
-        this.gameOverSong.volume = 0.2;
-        this.backgroundMusic.loop = true;
-    }
 
-    startMusic() {
-        this.backgroundMusic.play();
-    }
-    stopMusic() {
-        this.backgroundMusic.pause();
-        this.backgroundMusic.currentTime = 0;
-    }
-    victory() {
-        this.stopMusic();
-        this.victorySong.play();
-    }
-    gameOver() {
-        this.stopMusic();
-        this.gameOverSong.play();
+
+
+
+
+
+
+//================================================================
+
+
+
+
+// FLIP CARDS
+function cardFlip() {
+    if (lockBoard) return;
+
+    // double click on the card
+    if (this === firstCard) return;
+    this.classList.add("flip");
+    if (!theCardIsFlipped) {
+        // first click
+        theCardIsFlipped = true;
+        firstCard = this;
+    } else {
+        // second click
+        theCardIsFlipped = false;
+        secondCard = this;
+        cardMatch()
+
     }
 }
 
-
-
-// GRABBING OVERLAYS and CARD FROM THE DOM 
-function ready() {
-    let overlaysArray = Array.from(document.querySelectorAll(".overlay-text"));
-    let cardsArray = Array.from(document.querySelectorAll(".memory-card"));
-    let game;
-
-    const pathName = window.location.pathname;
-    const gameType = pathName.indexOf("easy") !== -1 ? "EASY" :
-        pathName.indexOf("hard") !== -1 ? "HARD" : "MEDIUM";
-
-    if (gameType === "EASY") {
-        game = new FlipAndTime(120, cardsArray);
-    } else if (gameType === "MEDIUM") {
-        game = new FlipAndTime(90, cardsArray);
-    } else if (gameType === "HARD") {
-        game = new FlipAndTime(60, cardsArray);
+function cardMatch() {
+    if (firstCard.dataset.framework === secondCard.dataset.framework) {
+        disabled();
+    } else {
+        notFlipped();
     }
+}
 
+function disabled() {
+    firstCard.removeEventListener('click', cardFlip);
+    secondCard.removeEventListener('click', cardFlip);
+    resetBoard();
+}
 
-    overlaysArray.forEach(overlay => {
-        overlay.addEventListener('click', () => {
-            overlay.classList.remove("visible");
-            //let audioController = new AudioController();
-            //audioController.startMusic();
-            game.startGame();
-        });
-    });
-
-    cardsArray.forEach(card => {
-        card.addEventListener('click', () => {
-            game.flipCard(card);
-        });
-    });
-
-    //================================================================
-
-    const cards = document.querySelectorAll('.memory-card');
-
-    let theCardIsFlipped;
-    let firstCard, secondCard;
-    // lockBoard = not to match another pair before the first pair matches (unflips) or unmatches (flips back)
-    let lockBoard = false;
-
-
-    // FLIP CARDS
-    function cardFlip() {
-        if (lockBoard) return;
-
-        // double click on the card
-        if (this === firstCard) return;
-        this.classList.add("flip");
-        if (!theCardIsFlipped) {
-            // first click
-            theCardIsFlipped = true;
-            firstCard = this;
-        } else {
-            // second click
-            theCardIsFlipped = false;
-            secondCard = this;
-            cardMatch()
-
-        }
-    }
-
-    function cardMatch() {
-        if (firstCard.dataset.framework === secondCard.dataset.framework) {
-            disabled();
-        } else {
-            notFlipped();
-        }
-    }
-
-    function disabled() {
-        firstCard.removeEventListener('click', cardFlip);
-        secondCard.removeEventListener('click', cardFlip);
-        resetBoard();
-    }
-
-    function notFlipped() {
-        lockBoard = true;
-        setTimeout(() => {
-            firstCard.classList.remove('flip');
-            secondCard.classList.remove('flip');
-            lockBoard = false;
-            resetBoard();
-        }, 700);
-    }
-
-    function resetBoard() {
-        theCardIsFlipped = false;
+function notFlipped() {
+    lockBoard = true;
+    setTimeout(() => {
+        firstCard.classList.remove('flip');
+        secondCard.classList.remove('flip');
         lockBoard = false;
-        firstCard = null;
-        secondCard = null;
-    }
+        resetBoard();
+    }, 700);
+}
+
+function resetBoard() {
+    theCardIsFlipped = false;
+    lockBoard = false;
+    firstCard = null;
+    secondCard = null;
+}
 
 
-    //This function will be exacuted right after its definition
+//This function will be exacuted right after its definition
 
-    cards.forEach(card => card.addEventListener('click', cardFlip));
+cards.forEach(card => card.addEventListener('click', cardFlip));
 }
 
 
